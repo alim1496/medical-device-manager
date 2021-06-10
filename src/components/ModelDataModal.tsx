a21abe4167db5c99c11e1f349e0aad615a8409f1
alim1496 <alim1496@gmail.com>
2021-06-10 11:23:15 +0600

    upto add data

--- src/components/ModelDataModal.tsx
+++ src/components/ModelDataModal.tsx
@@ -16,33 +16,79 @@ interface DataItem {
 }
 
 const ModelDataModal: FC = () => {
-  const { isModalOpen, brand, model } = useContext(DeviceContext);
+  const { isModalOpen, brand, model, updateModalOpen } = useContext(
+    DeviceContext
+  );
+  const [loading, setLoading] = useState(false);
   const [items, setItems] = useState<DataItem[]>([]);
 
   useEffect(() => {
     if (isModalOpen && brand !== "" && model !== "") {
+      setLoading(true);
       axios
         .get(
           `http://163.47.115.230:30000/api/overview/modeldata/${brand}/${model}`,
           config
         )
         .then(({ data }) => {
+          setLoading(false);
           setItems(data);
         })
         .catch((err) => {
+          setLoading(false);
           console.log(err);
         });
     }
   }, [isModalOpen]);
 
+  const closeModal = () => {
+    setItems([]);
+    updateModalOpen(false);
+  };
+
   return (
     <div className={`model-modal-container ${isModalOpen ? "show" : "hide"}`}>
-      {items &&
+      <p>
+        <b>{model} Data List</b>
+      </p>
+      {loading && <div className="main-loader" />}
+      {!loading && items && items.length === 0 && (
+        <p>This model does not have any data</p>
+      )}
+      {!loading &&
+        items &&
         items.map((item, index) => (
-          <div key={index}>
-            <span>{item.DataType}</span>
-          </div>
+          <>
+            <div key={index} className="single-item">
+              <span>
+                <b>Data Type&nbsp;</b>
+                {item.DataType}
+              </span>
+              <span>
+                <b>Name&nbsp;</b>
+                {item.Name}
+              </span>
+              <span>
+                <b>Display Name&nbsp;</b>
+                {item.DisplayName}
+              </span>
+              <span>
+                <b>Description&nbsp;</b>
+                {item.Description}
+              </span>
+              <span>
+                <b>Protocol Order&nbsp;</b>
+                {item.ProtocolOrder !== null ? item.ProtocolOrder : "Not Found"}
+              </span>
+            </div>
+            <hr />
+          </>
         ))}
+      {!loading && (
+        <button type="button" onClick={closeModal} className="close-btn">
+          Close
+        </button>
+      )}
     </div>
   );
 };
